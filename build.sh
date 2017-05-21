6f7856ae69ffbab0dd27ac5296aad2bc13199b29
Simon Erhardt <me@rootlogin.ch>
2017-05-21 22:40:59 +0200

    minimal build

--- build.sh
+++ build.sh
@@ -10,6 +10,39 @@ echo "Load NBD module and connect image..."
 modprobe nbd max_part=16
 qemu-nbd -c /dev/nbd0 debian.qcow2
 
+echo "Create swap and root partition..."
+sfdisk /dev/nbd0 -D -uM << EOF
+,512,82
+;
+EOF
+
+sleep 5
+echo "Format everything..."
+mkswap /dev/nbd0p1
+mkfs.ext3 /dev/nbd0p2
+mount /dev/nbd0p2 /mnt/
+
+echo "Install debian..."
+debootstrap --include=less,locales-all,vim,sudo stable /mnt http://ftp.ch.debian.org/debian
+
+echo "Mount /dev into image..."
+mount --bind /dev/ /mnt/dev
+
+echo "Chroot inside image and bootstrap..."
+LANG=C chroot /mnt /bin/bash -x <<'EOF'
+mount -t proc none /proc
+mount -t sysfs none /sys
+
+apt-get install linux-image-amd64 grub
+grub-install /dev/nbd0
+update-grub
+
+umount /proc/ /sys/ /dev/
+EOF
+
+echo "Fix grub..."
+grub-install /dev/nbd0 --root-directory=/mnt --modules="biosdisk part_msdos"
+
 echo "Disconnect image and unload NBD module..."
 qemu-nbd -d /dev/nbd0
 rmmod nbd
