bd1149136594e724c567b72796cb840c6dee5a9c
Simon Erhardt <me@rootlogin.ch>
2017-05-21 23:31:54 +0200

    safer mount & unmount

--- build.sh
+++ build.sh
@@ -19,18 +19,18 @@ sleep 5
 echo "Format everything..."
 mkswap /dev/nbd0p1
 mkfs.ext3 /dev/nbd0p2
-mount /dev/nbd0p2 /mnt/
+mount /dev/nbd0p2 /mnt
 
 echo "Install debian..."
 debootstrap --include=less,locales-all,vim,sudo stable /mnt http://ftp.ch.debian.org/debian
 
-echo "Mount /dev into image..."
-mount --bind /dev/ /mnt/dev
+echo "Mount image..."
+mount --bind /dev /mnt/dev
+mount -t proc none /mnt/proc
+mount -t sysfs none /mnt/sys
 
 echo "Chroot inside image and bootstrap..."
 LANG=C chroot /mnt /bin/bash -e -x <<'EOF'
-mount -t proc none /proc
-mount -t sysfs none /sys
 
 DEBIAN_FRONTEND=noninteractive apt-get install -y \
   linux-image-amd64 \
@@ -38,12 +38,18 @@ DEBIAN_FRONTEND=noninteractive apt-get install -y \
 
 grub-install /dev/nbd0
 update-grub
-
-umount /proc/ /sys/ /dev/
 EOF
 
 echo "Fix grub..."
 grub-install /dev/nbd0 --root-directory=/mnt --modules="biosdisk part_msdos"
+sleep 5
+
+echo "Unmount everything..."
+umount /mnt/sys
+umount /mnt/proc
+umount /mnt/dev
+umount /mnt
+sleep 5
 
 echo "Disconnect image and unload NBD module..."
 qemu-nbd -d /dev/nbd0
