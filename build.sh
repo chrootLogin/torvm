62bb39e31c087f840186154deb640f92e2b64e73
Simon Erhardt <me@rootlogin.ch>
2017-05-21 22:11:22 +0200

    test loading of qemu image

--- build.sh
+++ build.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-echo "Hello World!"
+set -e
+set -x
+
+echo "Create VM image..."
+qemu-img create -f qcow2 debian.qcow2 2G
+
+echo "Load NBD module and connect image..."
+modprobe nbd max_part=16
+qemu-nbd -c /dev/nbd0 debian.qcow2
+
+echo "Disconnect image and unload NBD module..."
+qemu-nbd -d /dev/nbd0
+rmmod nbd
