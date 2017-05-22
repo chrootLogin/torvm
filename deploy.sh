4935d3e03b8319d12eddab7102b7389fabf5eafa
Simon Erhardt <me+github@rootlogin.ch>
2017-05-22 10:48:08 +0200

    fix path

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ set -x
 gzip ./torvm.qcow2
 gzip ./torvm.vmdk
 
-curl --upload-file ./torvm.qcow2.bz2 https://transfer.sh/torvm-$BRANCH_NAME.qcow2.gz
-curl --upload-file ./torvm.vmdk.bz2 https://transfer.sh/torvm-$BRANCH_NAME.vmdk.gz
+curl --upload-file ./torvm.qcow2.gz https://transfer.sh/torvm-$BRANCH_NAME.qcow2.gz
+curl --upload-file ./torvm.vmdk.gz https://transfer.sh/torvm-$BRANCH_NAME.vmdk.gz
