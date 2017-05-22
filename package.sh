4eddc01d96e81bfdb1e787c30b4a6d45334c0f43
Simon Erhardt <me@rootlogin.ch>
2017-05-22 21:55:43 +0200

    add more options to vmx file

--- package.sh
+++ package.sh
@@ -22,15 +22,43 @@ echo "Create VMware package"
 cat << EOF > TorVM.vmware/TorVM.vmx
 config.version = "8"
 virtualHW.version = "7"
-guestOS = "otherlinux-64"
-numvcpus = "1"
+displayName = "TorVM"
+guestOS = "other26xlinux-64"
+numvcpus = "2"
 memsize = "1024"
+
+vmci0.present = "TRUE"
+
+floppy0.present = "FALSE"
 
 scsi0.present = "TRUE"
 scsi0.sharedBus = "none"
 scsi0.virtualDev = "lsilogic"
 scsi0:0.present = "TRUE"
 scsi0:0.fileName = "TorVM.vmdk"
+scsi0:0.deviceType = "scsi-hardDisk"
+
+ide1:0.present = "FALSE"
+
+pciBridge0.present = "TRUE"
+pciBridge4.present = "TRUE"
+pciBridge4.virtualDev = "pcieRootPort"
+pciBridge4.functions = "8"
+pciBridge5.present = "TRUE"
+pciBridge5.virtualDev = "pcieRootPort"
+pciBridge5.functions = "8"
+pciBridge6.present = "TRUE"
+pciBridge6.virtualDev = "pcieRootPort"
+pciBridge6.functions = "8"
+pciBridge7.present = "TRUE"
+pciBridge7.virtualDev = "pcieRootPort"
+pciBridge7.functions = "8"
+
+ethernet0.pciSlotNumber = "32"
+ethernet0.present = "TRUE"
+ethernet0.virtualDev = "e1000"
+ethernet0.networkName = "Inside"
+ethernet0.generatedAddressOffset = "0"
 EOF
 zip -9 -r torvm-vmware.zip vmware
 rm -rf vmware
