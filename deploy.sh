2b03e2c20b439a814b1e165ea30de46099e3c8a7
Simon Erhardt <me+github@rootlogin.ch>
2017-05-22 10:33:55 +0200

    change gzip

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-
 set -e
 set -x
 
-bzip2 ./torvm.qcow2
-bzip2 ./torvm.vmdk
+gzip ./torvm.qcow2
+gzip ./torvm.vmdk
 
-curl --upload-file ./torvm.qcow2.bz2 https://transfer.sh/torvm-$BRANCH_NAME.qcow2.bz2
-curl --upload-file ./torvm.vmdk.bz2 https://transfer.sh/torvm-$BRANCH_NAME.vmdk.bz2
+curl --upload-file ./torvm.qcow2.bz2 https://transfer.sh/torvm-$BRANCH_NAME.qcow2.gz
+curl --upload-file ./torvm.vmdk.bz2 https://transfer.sh/torvm-$BRANCH_NAME.vmdk.gz
