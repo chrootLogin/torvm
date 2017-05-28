9d25c0087def1d153388e33980a9d9a16219d083
Simon Erhardt <me@rootlogin.ch>
2017-05-28 22:20:11 +0200

    fixed firewall script

--- root/usr/local/bin/tor-firewall.sh
+++ root/usr/local/bin/tor-firewall.sh
@@ -9,10 +9,16 @@ TRANS_PORT="9040"
 iptables -F
 iptables -t nat -F
 
+iptables -P INPUT ACCEPT
+iptables -P OUTPUT DROP
+
+iptables -A OUTPUT -d 127.0.0.1/8 -j ACCEPT
+
+iptables -A OUTPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -A OUTPUT -m owner --uid-owner $TOR_UID -j ACCEPT
+
 iptables -t nat -A OUTPUT -m owner --uid-owner $TOR_UID -j RETURN
 iptables -t nat -A OUTPUT -p udp --dport 53 -j REDIRECT --to-ports 53
 iptables -t nat -A OUTPUT -p tcp --syn -j REDIRECT --to-ports $TRANS_PORT
 
-iptables -A OUTPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
-iptables -A OUTPUT -m owner --uid-owner $TOR_UID -j ACCEPT
 iptables -A OUTPUT -j REJECT
